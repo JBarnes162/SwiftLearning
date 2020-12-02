fc2af8b419aa047dca2d241340df939de496abe8
Jackson <jackson.barnes162@gmail.com>
2020-12-02 14:49:56 +0000

    End of interpolation, start of functions

--- StringInterpolation.playground/Contents.swift
+++ StringInterpolation.playground/Contents.swift
@@ -1,1 +1,15 @@
+//String Interpolation
+
+//Example Data
+var trackName = "Ambre"
+var artistName = "Nils Frahm"
+var duration = 228
+
+//...
+trackName = "Ombre"
+
+let message = "Now playing \(trackName) by \(artistName) which is \(duration) long"
+
+print("Now playing \(trackName) by \(artistName) which is \(duration/60):\(duration%60) long")
+print(message)
 
