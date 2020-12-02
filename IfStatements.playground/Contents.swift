962b12d740096844f1eb5485fe2abe5622b07747
Jackson <jackson.barnes162@gmail.com>
2020-12-02 14:03:23 +0000

    End of If, start of switch statements

--- IfStatements.playground/Contents.swift
+++ IfStatements.playground/Contents.swift
@@ -1,1 +1,19 @@
+var conditonOne = 22
+var conditionTwo = 23
 
+if (conditonOne > conditionTwo){
+    print("One is greater than two")
+} else{
+    print("Two is greater than one")
+}
+
+var score = 100
+
+//parenthesis are not required
+if score > 10{
+    print("Score is greater than 10")
+} else{
+    print("Score is not greater than 10")
+}
+
+//conditions must make a true or false output
