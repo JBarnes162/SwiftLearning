a62bf0028d36f8e05ed7b9c4aecdca26396079b9
Jackson <jackson.barnes162@gmail.com>
2020-12-04 21:24:36 +0000

    Done with extenstion
    Can add additional functionality to classes for example, can add a method to string that can be used by every string through out my code

--- Extentions.playground/Contents.swift
+++ Extentions.playground/Contents.swift
@@ -1,3 +1,26 @@
-import UIKit
+// Adding functionality with extensions
 
-var str = "Hello, playground"
+extension String {
+    func removeSpaces() -> String{
+        let filteredCharacters = self.filter{ $0 != " " }
+        return String(filteredCharacters)
+    }
+}
+
+var babiesNumber = 0
+babiesNumber += 20+30+50+50+50+50+50+50+50+100
+print(babiesNumber)
+var babiesNewNumber = 0
+babiesNewNumber += (50*4) + (100*4) + (150*4) + (200*3) + (250*3) + 1000
+print(babiesNewNumber)
+
+//string
+let album = "Decks and drums and rock and roll"
+let scriptio = "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet"
+let phrase = "Love is new here"
+
+print(album.removeSpaces())
+print(scriptio.removeSpaces())
+print(phrase.removeSpaces())
+
+album.uppercased()
