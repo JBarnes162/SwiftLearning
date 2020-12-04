c3bf4efc08e45b7dd56ab108bfcee05d54ed899b
Jackson <jackson.barnes162@gmail.com>
2020-12-04 21:08:25 +0000

    Inheritance finished, Extensions time
    Super class used to share contents with subclasses,can use keyword final to private certain classes or prevent methods within classes being overwritten

--- Inheritance.playground/Contents.swift
+++ Inheritance.playground/Contents.swift
@@ -1,1 +1,41 @@
+//Inheritance
 
+ class Appliance { //final keyword would stop any class inheriting from this class
+    //properties
+    var model: String
+    var manufacturer: String
+    
+    //Initialise
+    init() {
+        self.model = "default"
+        self.manufacturer = "default"
+    }
+    
+    //Methods
+    final func printDetails(){
+        print("Make: \(self.manufacturer), Model: \(self.model) ")
+    }
+}
+
+//define a new classe and inherit from Appliance
+
+class Toaster: Appliance {
+    //new property
+    var slices: Int
+    
+    //new initializer
+    override init(){
+        self.slices = 2
+        super.init()
+    }
+    //new method
+    func toast(){
+        print("Toasting...")
+    }
+}
+
+var myToaster = Toaster()
+myToaster.manufacturer = "Next"
+myToaster.model = "Nuclear"
+myToaster.printDetails()
+myToaster.toast()
